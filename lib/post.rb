0d8ef98b31beb2f600667bf2a14e17de583439d0
Gregory Losco <gtlosco@gmail.com>
2019-04-18 02:19:38 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,2 +1,16 @@
 class Post 
+  
+  attr_accessor :title, :author
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    @@all << title
+  end 
+  
+  def self.all
+    @@all
+  end
+  
 end 
